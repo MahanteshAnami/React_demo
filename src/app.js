d37c6911203a53924a4f7d8fd4d467d349a156cd
A.Aditya <adityaanupindi@gmail.com>
2016-09-15 06:16:21 +0000

    working

--- src/app.js
+++ src/app.js
@@ -1,63 +1,23 @@
-var React = require('react')
-  , ReactDom = require('react-dom')
-  , ReactRouter = require('react-router')
-
-var Comments = require('./comments')
-  , Login = require('./login')
-
-var Route = ReactRouter.Route
-  , Router = ReactRouter.Router
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, Link,browserHistory, IndexRoute } from 'react-router';
 
 
-var CommentsApp = React.createClass({
-  mixins: [ReactRouter.History],
 
-  signOut: function(e) {
-    e.preventDefault()
-    delete window.User
-    this.history.pushState(null, "/login")
-  },
-
-  render: function() {
-    if (window.User) {
-      var navigation = (
-        <nav className="App-navigation">
-          <a href="#/">All</a>
-          <a href="#" onClick={this.signOut}>Sign Out</a>
-        </nav>
-      )
-    }
-
+class Contacts extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
     return (
-      <section className="CommentsApp">
-        <h1>Comments</h1>
-        <h2>Share what you think about the world</h2>
-
-        {navigation}
-
-        {this.props.children}
-      </section>
-    )
-  }
-})
-
-function requireLogin(nextState, replaceState) {
-  if (!window.User) {
-    replaceState({ nextPathname: nextState.location.pathname }, '/login')
+      <div>My example project</div>
+    );
   }
 }
 
-function requireLogout(nextState, replaceState) {
-  if (window.User) {
-    replaceState({ nextPathname: nextState.location.pathname }, '/')
-  }
-}
+export default Contacts;
 
-ReactDom.render((
-  <Router>
-    <Route path="/" component={CommentsApp}>
-      <Route path="login" component={Login} onEnter={requireLogout} />
-      <Route path="comments" component={Comments} onEnter={requireLogin} />
-    </Route>
-  </Router>
-), document.getElementById("app"))
+ReactDOM.render(
+  <Contacts />,
+  document.getElementById('app')
+);
